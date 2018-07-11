4e60efccb4f05eb500580d341af8613a29f076a9
aquariuz <gpetiaa@gmail.com>
2018-07-11 19:59:48 +0300

    arranged around

--- Class-Inheritance-and-Prototypes-Exercises/Mixins.js
+++ Class-Inheritance-and-Prototypes-Exercises/Mixins.js
@@ -1,35 +1,28 @@
 function createMixins() {
-    function computerQualityMixin (classToExtend) {
-        function getQuality() {
+    function computerQualityMixin(classToExtend) {
+        classToExtend.prototype.getQuality = function () {
             return (this.processorSpeed + this.ram + this.hardDiskSpace) / 3;
-        }
-        
-        function isFast() {
-            return this.processorSpeed > (this.ram / 4);
-        }
-        
-        function isRoomy() {
-            return this.hardDiskSpace  > Math.floor(this.ram * this.processorSpeed)
-        }
+        };
 
-        classToExtend.prototype.getQuality = getQuality;
-        classToExtend.prototype.isFast = isFast;
-        classToExtend.prototype.isRoomy = isRoomy;
+        classToExtend.prototype.isFast = function () {
+            return this.processorSpeed > (this.ram / 4);
+        };
+
+        classToExtend.prototype.isRoomy = function () {
+            return this.hardDiskSpace > Math.floor(this.ram * this.processorSpeed)
+        };
     }
 
     function styleMixin(classToExtend) {
-        function isFullSet() {
+        classToExtend.prototype.isFullSet = function () {
             return this.manufacturer === this.keyboard.manufacturer && this.manufacturer === this.monitor.manufacturer;
-        }
-        
-        function isClassy() {
+        };
+
+        classToExtend.prototype.isClassy = function () {
             return this.battery.expectedLife >= 3
                 && (this.color === "Silver" || this.color === "Black")
                 && this.weight < 3;
         }
-
-        classToExtend.prototype.isFullSet = isFullSet;
-        classToExtend.prototype.isClassy = isClassy;
     }
 
     return {
@@ -37,3 +30,25 @@ function createMixins() {
         styleMixin
     }
 }
+
+let mixins = createMixins();
+let computerQualityMixin = mixins.computerQualityMixin;
+let styleMixin = mixins.styleMixin;
+
+class Computer {
+    constructor(manufacturer, processorSpeed, ram, hardDiskSpace) {
+        // if (new.target === Computer) {
+        //     throw new Error("Abstract Class!")
+        // }
+
+        this.manufacturer = manufacturer;
+        this.processorSpeed = processorSpeed;
+        this.ram = ram;
+        this.hardDiskSpace = hardDiskSpace;
+    }
+}
+
+computerQualityMixin(Computer);
+styleMixin(Computer);
+let comp = new Computer("HP", 2.3, 4, 1000);
+console.log(comp.isRoomy());
