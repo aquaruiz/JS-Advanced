eaa9eb83fa6258b48ecfc3ec9531557391722b10
aquariuz <gpetiaa@gmail.com>
2018-07-11 19:21:04 +0300

    changed default sort function

--- Modules-Lab/SortMap/helper-functions.js
+++ Modules-Lab/SortMap/helper-functions.js
@@ -1,4 +1,4 @@
-function sortMap(map, sortFunc = (a, b) => a[0] - b[0]) {
+function sortMap(map, sortFunc = (a, b) => a[0].localeCompare(b[0])) {
     let entries = Array.from(map.entries()) // [dimentional array - key value]
         .sort(sortFunc);
 
@@ -14,7 +14,16 @@ let map = new Map();
 map.set("300","Pesho");
 map.set("10","Gosho");
 map.set("7","Aleks");
-console.log(sortMap(map, (a, b) => a[0].localeCompare(b[0])));
+console.log(sortMap(map, (a, b) => a[0] - (b[0])));
 console.log(sortMap(map));
+
+map = new Map();
+map.set("Stefan",true);
+map.set("Azazel",false);
+map.set("Bismoth",false);
+map.set("Balrog",true);
+map.set("Martel",true);
+let sortedMap = sortMap(map);
+console.log(sortedMap);
 
 module.exports = sortMap;
